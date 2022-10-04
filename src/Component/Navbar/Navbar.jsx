64669d3ecbfda27594853ec721216e6e22c66bee
ledinhhieu22122002 <ledinhhieu1212003@gmail.com>
2022-10-04 21:03:54 +0700

    uuk

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -13,18 +13,18 @@ const Navbar = (props) => {
     return (
         <nav className="navbar navbar-expand-lg">
             <div className="container">
-                <Link className={`navbar__logo navbar-brand`} to='/'>
+                <Link className={`navbar__logo navbar-brand`} to='/restaurant-react/'>
                     <img src={props.logo} alt="" />
                 </Link>
                 <div className="collapse navbar-collapse">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <LLink to='/' title='Home' />
+                        <LLink to='/restaurant-react/' title='Home' />
                         <LLink to='/restaurant-react/Menu' title='Menu' />
-                        <LLink to='/AboutUs' title='About Us' />
-                        <LLink to='/OurStory' title='Our Story' />
-                        <LLink to='/Blog' title='Blog' />
-                        <LLink to='/OurStory' title='Our Story' />
-                        <LLink to='/Contact' title='Contact' />
+                        <LLink to='/restaurant-react/AboutUs' title='About Us' />
+                        <LLink to='/restaurant-react/OurStory' title='Our Story' />
+                        <LLink to='/restaurant-react/Blog' title='Blog' />
+                        <LLink to='/restaurant-react/OurStory' title='Our Story' />
+                        <LLink to='/restaurant-react/Contact' title='Contact' />
                     </ul>
                     <div className="d-flex">
                         <Link className="navbar__clone_project" to="/clone ">
